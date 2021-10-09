fe1279b8a688c91193f0ecedcb68f7610be51589
Yogeshwar-Pawade <yogeshwar.pawade@gmail.com>
2021-10-09 09:41:41 +0530

    New

--- src/Pages/Hero.js
+++ src/Pages/Hero.js
@@ -1,11 +1,13 @@
 import React from "react";
+import About from "../components/About";
 import Heading from "../components/Heading";
 import "./Hero.css";
 
 function Hero() {
   return (
     <div className="hero">
-          <Heading></Heading>
+         <Heading></Heading> 
+          <About></About>
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="About">
+      <div className="Aboutleft"></div>
+      <div className="Aboutright">
+        <h1 className="Aboutheading">
+          Specialization in Artificial Intelligence & Machine Learning,
+        </h1>
+        <h1 className="Aboutheading">Highly Innovative Web Developer</h1>
+        <hr className="Aboutline"></hr>
+        <p className="Detail">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla
+          suscipit officiis hic, dolor pariatur odio deleniti nostrum aliquam,
+          qui quam aliquid harum culpa eveniet delectus, ipsum enim modi ullam
+          maxime!
+        </p>
+        <div>
+          <div>
+            <div></div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <button></button>
+      </div>
+    </div>
+  );
+}
+
+export default About;
