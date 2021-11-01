5859b12765bea7eca90a9508782376071f71514c
Yogeshwar-Pawade <yogeshwar.pawade@gmail.com>
2021-11-01 22:09:27 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from "react"; //rfce //npm start -to start
 import LeftSideBar from "./Pages/LeftSideBar";
 import TopBar from "./Pages/TopBar";
 import Hero from "./Pages/Hero";
+import MetaTags from "react-meta-tags";
 
 function App() {
   return (
     <div>
+      <MetaTags>
+        <meta name="viewport" content="width=1440px, initial-scale=1.0" />
+      </MetaTags>
       <LeftSideBar />
       <TopBar />
       <Hero />
